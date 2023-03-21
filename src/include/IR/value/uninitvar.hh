355a13cfc4b9d8d3b27af5f86eb55a8a0c037e2a
cocodery <chriswuyyy123@gmail.com>
2023-03-21 14:34:24 +0800

    [c2rv] add new value UnInitVar which only used in glb-var and param

--- src/include/IR/value/uninitvar.hh
+++ src/include/IR/value/uninitvar.hh
@@ -4,7 +4,13 @@
 
 class UnInitVar : public BaseValue {
 public:
+    UnInitVar(TypeID);
+    UnInitVar(ListTypePtr);
+
     void fixValue(TypeID) { return; }
+
+    static std::shared_ptr<UnInitVar> CreatePtr(TypeID);
+    static std::shared_ptr<UnInitVar> CreatePtr(ListTypePtr);
     
-    std::string toString() { return ""; }
+    std::string toString();
 };
---
+++ src/lib/IR/value/uninitvavr.cpp
@@ -0,0 +1,35 @@
+#include "uninitvar.hh"
+
+UnInitVar::UnInitVar(TypeID _tid) : BaseValue(_tid) {
+
+}
+
+UnInitVar::UnInitVar(ListTypePtr list_type) : BaseValue(list_type) {
+
+}
+
+std::shared_ptr<UnInitVar> UnInitVar::CreatePtr(TypeID _tid) {
+    return std::make_shared<UnInitVar>(_tid);
+}
+
+std::shared_ptr<UnInitVar> UnInitVar::CreatePtr(ListTypePtr list_type) {
+    return std::make_shared<UnInitVar>(list_type);
+}
+
+std::string UnInitVar::toString() {
+    BaseTypePtr base_tpye = this->getBaseType();
+    
+    std::stringstream ss;
+    ss << base_tpye->toString();
+    ss << " -> ";
+    if (base_tpye->ArrayType()) {
+        ss << "zeroinitializer";
+    } else {
+        if (base_tpye->IntType()) {
+            ss << "i32 0";
+        } else if (base_tpye->FloatType()) {
+            ss << "float 0";
+        }
+    }
+    return ss.str();
+}
