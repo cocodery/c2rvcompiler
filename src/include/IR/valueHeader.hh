6c81dbc580c142213eedfae2abff3bacc4d2a3d1
cocodery <chriswuyyy123@gmail.com>
2023-03-22 23:22:33 +0800

    [c2rv] add interface for unaryOp and binOp for BaseValuePtr

--- src/include/IR/valueHeader.hh
+++ src/include/IR/valueHeader.hh
@@ -8,8 +8,22 @@
 #include "value/uninitvar.hh"
 #include "value/variable.hh"
 
+#include "function/basicblock.hh"
+
 using BaseValuePtr  = std::shared_ptr<BaseValue>;
 using ConstantPtr   = std::shared_ptr<Constant>;
 using ConstArrayPtr = std::shared_ptr<ConstArray>;
 using UnInitVarPtr  = std::shared_ptr<UnInitVar>;
 using VariablePtr   = std::shared_ptr<Variable>;
+
+namespace Value {
+
+bool isConstant(BaseValuePtr);
+
+bool bothConstant(BaseValuePtr, BaseValuePtr);
+
+BaseValuePtr unaryOperate(std::string &, BaseValuePtr, BlockPtr);
+
+BaseValuePtr binaryOperate(std::string &, BaseValuePtr, BaseTypePtr, BlockPtr);
+
+}
---
+++ src/lib/IR/valueHeader.cpp
@@ -0,0 +1,20 @@
+#include "valueHeader.hh"
+
+bool Value::isConstant(BaseValuePtr value) {
+    BaseTypePtr base_type = value->getBaseType();
+    return (base_type->ConstType() && !base_type->ArrayType());
+}
+
+bool Value::bothConstant(BaseValuePtr value1, BaseValuePtr value2) {
+    return isConstant(value1) && isConstant(value2);
+}
+
+BaseValuePtr Value::unaryOperate(std::string &op, BaseValuePtr value, BlockPtr block) {
+    if (op == "+") return value;
+
+    if (isConstant(value)) {
+        ConstantPtr constant = std::dynamic_pointer_cast<Constant>(value);
+        return constant->unaryOperate(op);
+    }
+    assert(0);
+}
