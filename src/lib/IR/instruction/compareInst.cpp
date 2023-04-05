bbdc567a23d4eb43fead8bf5ed9c88a119f064a3
cocodery <chriswuyyy123@gmail.com>
2023-04-05 23:18:53 +0800

    [c2rv] check type-equality of lhs/rhs, add OP_GTH OP_GEQ

--- src/lib/IR/instruction/compareInst.cpp
+++ src/lib/IR/instruction/compareInst.cpp
@@ -7,8 +7,9 @@
 ICmpInst::ICmpInst(BaseValuePtr _res, OpCode _cond, BaseValuePtr _lhs, BaseValuePtr _rhs)
     : result(_res), cond(_cond), lhs(_lhs), rhs(_rhs) {
     assert(_res->getBaseType()->checkType(BOOL, VARIABLE));
-    assert(_lhs->getBaseType()->checkType(BOOL | INT));
-    assert(_rhs->getBaseType()->checkType(BOOL | INT));
+    TypeID tid_lhs = _lhs->getBaseType()->getMaskedType(BOOL | INT);
+    TypeID tid_rhs = _rhs->getBaseType()->getMaskedType(BOOL | INT);
+    assert(tid_lhs == tid_rhs);
 }
 
 ICmpInstPtr ICmpInst::CreatePtr(BaseValuePtr _res, OpCode _cond, BaseValuePtr _lhs, BaseValuePtr _rhs) {
@@ -18,15 +19,15 @@ ICmpInstPtr ICmpInst::CreatePtr(BaseValuePtr _res, OpCode _cond, BaseValuePtr _l
 std::string ICmpInst::tollvmIR() {
     std::stringstream ss;
     ss << result->tollvmIR() << " = icmp ";
-    if (cond == OP_LTH) {
-        ss << "slt";
-    } else if (cond == OP_LEQ) {
-        ss << "sle";
-    } else if (cond == OP_EQU) {
-        ss << "eq";
-    } else if (cond == OP_NEQ) {
-        ss << "ne";
-    } else { assert(0); }
+    switch (cond) {
+        case OP_LTH: ss << "slt"; break;
+        case OP_LEQ: ss << "sle"; break;
+        case OP_GTH: ss << "sgt"; break;
+        case OP_GEQ: ss << "sge"; break;
+        case OP_EQU: ss << "eq" ; break;
+        case OP_NEQ: ss << "ne" ; break;
+        default : assert(0)   ; break;
+    }
     ss << " " << lhs->getBaseType()->tollvmIR() << " " << lhs->tollvmIR() << ", " << rhs->tollvmIR();
     return ss.str();
 }
@@ -49,15 +50,15 @@ FCmpInstPtr FCmpInst::CreatePtr(BaseValuePtr _res, OpCode _cond, BaseValuePtr _l
 std::string FCmpInst::tollvmIR() {
     std::stringstream ss;
     ss << result->tollvmIR() << " = fcmp ";
-    if (cond == OP_LTH) {
-        ss << "olt";
-    } else if (cond == OP_LEQ) {
-        ss << "ole";
-    } else if (cond == OP_EQU) {
-        ss << "oeq";
-    } else if (cond == OP_NEQ) {
-        ss << "one";
-    } else { assert(0); }
+    switch (cond) {
+        case OP_LTH: ss << "olt"; break;
+        case OP_LEQ: ss << "ole"; break;
+        case OP_GTH: ss << "ogt"; break;
+        case OP_GEQ: ss << "oge"; break;
+        case OP_EQU: ss << "oeq"; break;
+        case OP_NEQ: ss << "one"; break;
+        default : assert(0)   ; break;
+    }
     ss << " float " << lhs->tollvmIR() << ", " << rhs->tollvmIR();
     return ss.str();
 }
