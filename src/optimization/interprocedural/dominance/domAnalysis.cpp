6145d97b466fd818f5c362285dd4cdc7de5b4822
cocodery <chriswuyyy123@gmail.com>
2023-04-26 22:53:51 +0800

    [c2rv] namespace DominanceAnalysis package related function

--- src/optimization/interprocedural/dominance/domAnalysis.cpp
+++ src/optimization/interprocedural/dominance/domAnalysis.cpp
@@ -1,8 +1,7 @@
 #include "domAnalysis.hh"
 
-DomAnalysis::DomAnalysis(CfgNodePtr _entry, CfgNodeList(_allNodes)) : entry(_entry), allNodes(_allNodes) {}
-
-void DomAnalysis::DominanceAnalysis() {
+void DominanceAnalysis::ComputeDominanceInfo(CfgNodePtr entry, CfgNodeList allNodes) {
+    // dominator-set
     entry->InsertDominator(entry);
     DominatorSet allNodeSet = DominatorSet(allNodes.begin(), allNodes.end());
 
@@ -13,14 +12,14 @@ void DomAnalysis::DominanceAnalysis() {
 
     auto &&IntersectPredecessorsDom = [&](CfgNodePtr node) {
         DominatorSet dom_set = allNodeSet;
-
-        for (auto &&pred : node->GetPredcessors()) {
+        auto &&predcessors = node->GetPredcessors();
+        std::for_each(predcessors.begin(), predcessors.end(), [&dom_set](const auto &pred) {
             DominatorSet temp = DominatorSet();  // To Collect Intersection
             DominatorSet dom_set_pred = pred->GetDominatorSet();
             std::set_intersection(dom_set.begin(), dom_set.end(), dom_set_pred.begin(), dom_set_pred.end(),
                                   std::inserter(temp, temp.begin()));
             dom_set = temp;
-        }
+        });
         dom_set.insert(node);
         return dom_set;
     };
@@ -28,10 +27,8 @@ void DomAnalysis::DominanceAnalysis() {
     bool changed = true;
     while (changed) {
         changed = false;
-
         for (auto &&node : allNodes) {
             if (node == entry) continue;
-
             DominatorSet temp = IntersectPredecessorsDom(node);
             if (temp != node->GetDominatorSet()) {
                 node->SetDominatorSet(temp);
@@ -40,51 +37,35 @@ void DomAnalysis::DominanceAnalysis() {
         }
     }
 
-    auto &&ComputeImmediateDominator = [&](CfgNodePtr node) {
+    auto &&ComputeImmediateDominator = [&entry](const auto &node) {
         DominatorSet dominator_set = node->GetDominatorSet();
         for (auto &&dominator : dominator_set) {
             if (dominator == node) continue;
-
             DominatorSet dom_set_of_dom = dominator->GetDominatorSet();
             dom_set_of_dom.insert(node);
-            if (dom_set_of_dom == dominator_set) {
-                return dominator;
-            }
+            if (dom_set_of_dom == dominator_set) return dominator;
         }
         // unreachable!
-        assert(false);
+        assert(node == entry);
+        return CfgNodePtr(nullptr);
     };
 
-    for (auto &&node : allNodes) {
-        if (node == entry) continue;
+    std::for_each(allNodes.begin(), allNodes.end(), [&ComputeImmediateDominator](const auto &node) {
         node->SetImmediateDominator(ComputeImmediateDominator(node));
-    }
+    });
 }
 
-void DomAnalysis::DominanceFrontier() {
-    for (auto &&node : allNodes) {
-        node->GetDomFrontier().clear();
-    }
-
-    for (auto &&node : allNodes) {
+void DominanceAnalysis::ComputeDominanceFrontier(CfgNodeList allNodes) {
+    std::for_each(allNodes.begin(), allNodes.end(), [](const auto &node) { node->GetDomFrontier().clear(); });
+    std::for_each(allNodes.begin(), allNodes.end(), [](const auto &node) {
         if (node->GetPredcessors().size() > 1) {
             for (auto &&pred : node->GetPredcessors()) {
-                CfgNodePtr runner = pred;
+                auto &&runner = pred;
                 while (runner != node->GetImmediateDominator()) {
                     runner->InsertDomFrontier(node);
                     runner = runner->GetImmediateDominator();
                 }
             }
         }
-    }
-}
-
-void DomAnalysis::DoDomAnalysis() {
-    DominanceAnalysis();
-    DominanceFrontier();
-}
-
-void DoDomAnalysis(CfgNodePtr _entry, CfgNodeList _allNodes) {
-    DomAnalysis dominance = DomAnalysis(_entry, _allNodes);
-    dominance.DoDomAnalysis();
+    });
 }
--- src/optimization/interprocedural/dominance/domAnalysis.hh
+++ src/optimization/interprocedural/dominance/domAnalysis.hh
@@ -5,19 +5,7 @@
 
 #include "IR.hh"
 
-class DomAnalysis {
-   private:
-    CfgNodePtr entry;
-    CfgNodeList allNodes;
-
-   public:
-    DomAnalysis(CfgNodePtr, CfgNodeList);
-    ~DomAnalysis() = default;
-
-    void DominanceAnalysis();
-    void DominanceFrontier();
-
-    void DoDomAnalysis();
-};
-
-void DoDomAnalysis(CfgNodePtr, CfgNodeList);
+namespace DominanceAnalysis {
+void ComputeDominanceInfo(CfgNodePtr, CfgNodeList);
+void ComputeDominanceFrontier(CfgNodeList);
+}  // namespace DominanceAnalysis
