56bd1c0ad3e35eab324252ee43b8300de46b210b
cocodery <chriswuyyy123@gmail.com>
2023-04-29 00:59:31 +0800

    [c2rv] SSA-Construction with global-variable error

---
+++ src/optimization/interprocedural/ssa/ssa.hh
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <list>
+#include <memory>
+#include <queue>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <vector>
+
+#include "IR.hh"
+#include "domAnalysis.hh"
+
+namespace StaticSingleAssignment {
+
+using ValueVector = std::vector<BaseValuePtr>;
+
+struct RenameData;
+using RenameDatePtr = std::shared_ptr<RenameData>;
+struct RenameData {
+    CfgNodePtr node;
+    CfgNodePtr pred;
+    ValueVector valuelist;
+
+    RenameData(CfgNodePtr, CfgNodePtr, ValueVector);
+
+    static RenameDatePtr CreatePtr(CfgNodePtr, CfgNodePtr, ValueVector);
+};
+
+void SSAConstruction(CfgNodePtr, CfgNodeList, size_t);
+}  // namespace StaticSingleAssignment
