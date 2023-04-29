f394015e5fd986aecb2931761125f4a423f2325e
cocodery <chriswuyyy123@gmail.com>
2023-04-30 01:50:41 +0800

    [c2rv] fix modify dom-set by referrence

--- src/pass/analysis/dominance/domAnalysis.cpp
+++ src/pass/analysis/dominance/domAnalysis.cpp
@@ -2,40 +2,48 @@
 
 void DominanceAnalysis::ComputeDominanceInfo(CfgNodePtr entry, CfgNodeList allNodes) {
     // dominator-set
-    entry->InsertDominator(entry);
     DominatorSet allNodeSet = DominatorSet(allNodes.begin(), allNodes.end());
 
-    for (auto &&node : allNodes) {
-        if (node == entry) continue;
-        node->SetDominatorSet(allNodeSet);
-    }
+    auto &&InitDominatorSet = [&entry, &allNodes, &allNodeSet]() {
+        entry->InsertDominator(entry);
 
-    auto &&IntersectPredecessorsDom = [&](CfgNodePtr node) {
-        DominatorSet dom_set = allNodeSet;
-        auto &&predcessors = node->GetPredcessors();
-        std::for_each(predcessors.begin(), predcessors.end(), [&dom_set](const auto &pred) {
-            DominatorSet temp = DominatorSet();  // To Collect Intersection
-            DominatorSet dom_set_pred = pred->GetDominatorSet();
-            std::set_intersection(dom_set.begin(), dom_set.end(), dom_set_pred.begin(), dom_set_pred.end(),
-                                  std::inserter(temp, temp.begin()));
-            dom_set = temp;
-        });
-        dom_set.insert(node);
-        return dom_set;
-    };
-
-    bool changed = true;
-    while (changed) {
-        changed = false;
         for (auto &&node : allNodes) {
             if (node == entry) continue;
-            DominatorSet temp = IntersectPredecessorsDom(node);
-            if (temp != node->GetDominatorSet()) {
-                node->SetDominatorSet(temp);
-                changed = true;
+            node->SetDominatorSet(allNodeSet);
+        }
+    };
+
+    auto &&ComputeDominatorSet = [&entry, &allNodes, &allNodeSet] {
+        auto &&IntersectPredecessorsDom = [&](CfgNodePtr node) {
+            DominatorSet dom_set = allNodeSet;
+            auto &&predcessors = node->GetPredcessors();
+            std::for_each(predcessors.begin(), predcessors.end(), [&dom_set](const auto &pred) {
+                DominatorSet temp = DominatorSet();  // To Collect Intersection
+                DominatorSet dom_set_pred = pred->GetDominatorSet();
+                std::set_intersection(dom_set.begin(), dom_set.end(), dom_set_pred.begin(), dom_set_pred.end(),
+                                      std::inserter(temp, temp.begin()));
+                dom_set = temp;
+            });
+            dom_set.insert(node);
+            return dom_set;
+        };
+
+        bool changed = true;
+        while (changed) {
+            changed = false;
+            for (auto &&node : allNodes) {
+                if (node == entry) continue;
+                DominatorSet temp = IntersectPredecessorsDom(node);
+                if (temp != node->GetDominatorSet()) {
+                    node->SetDominatorSet(temp);
+                    changed = true;
+                }
             }
         }
-    }
+    };
+
+    InitDominatorSet();
+    ComputeDominatorSet();
 
     auto &&ComputeImmediateDominator = [&entry](const auto &node) {
         DominatorSet dominator_set = node->GetDominatorSet();
@@ -60,7 +68,7 @@ void DominanceAnalysis::ComputeDominanceFrontier(CfgNodeList allNodes) {
     std::for_each(allNodes.begin(), allNodes.end(), [](const auto &node) {
         if (node->GetPredcessors().size() > 1) {
             for (auto &&pred : node->GetPredcessors()) {
-                auto &&runner = pred;
+                CfgNodePtr runner = pred;
                 while (runner != node->GetImmediateDominator()) {
                     runner->InsertDomFrontier(node);
                     runner = runner->GetImmediateDominator();
